b472316233253698ea4360180ff7d0ed41a1d07a
Vítor Norberto <vithor.norberto@gmail.com>
2020-12-19 15:42:14 -0300

    Adiciona dashboard inicial do terapeuta

---
+++ src/app/dashboard-terapeuta/dashboard-terapeuta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-terapeuta',
+  templateUrl: './dashboard-terapeuta.component.html',
+  styleUrls: ['./dashboard-terapeuta.component.css']
+})
+export class DashboardTerapeutaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
